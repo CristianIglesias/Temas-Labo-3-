bf0067c3cf420cd4cffa5d50c5efd4dd302b7481
ChiviFe <chivife@gmail.com>
2021-02-21 01:16:55 -0300

    Ya estaríamos para entregar

--- Vista_Lista_Productos.sql
+++ Vista_Lista_Productos.sql
@@ -6,18 +6,17 @@
  
  select p.*, tp.* from Producto as p join TipoProducto tp on tp.Id = p.IdTipo
 
-
- create view VW_ListaPromedioCompras
- as 
- select nombre, apellido, 
- (
-	select COUNT(*) from Pedidos as p where dp.IdUsuario=p.IdUsuario
- ) as 'Cantidad Compras',
- (
- select isnull(AVG(P.importe),0) from Pedidos as p where dp.IdUsuario=p.IdUsuario 
- ) as 'Promedio Compras'
- from DatosPersonales as dp
  
+create view VW_ListaPromedioCompras
+ as 
+ select IdUsuario,U.NombreUsuario, nombre, apellido, 
+ (select COUNT(*) from Pedidos as p where dp.IdUsuario=p.IdUsuario ) as 'CantidadCompras',
+ (select isnull(AVG(P.importe),0) from Pedidos as p where dp.IdUsuario=p.IdUsuario) as 'PromedioCompras'
+ from DatosPersonales as dp join Usuarios u ON u.Id=dp.IdUsuario
+ 
+
+ select Idusuario, NombreUsuario, nombre, Apellido, CantidadCompras, PromedioCompras from VW_ListaPromedioCompras
+
  select 
  AVG(*) from pedidos as p where IdUsuario = @
  from
